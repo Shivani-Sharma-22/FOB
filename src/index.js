5f1ed7cce31d5c26d25eb9d4250d2a8bc88d9790
shivani sharma <shivanisharma3081@gmail.com>
2025-07-08 21:44:44 +0530

    your update message

--- src/index.js
+++ src/index.js
@@ -54,6 +54,7 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
+app.options('*', cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
